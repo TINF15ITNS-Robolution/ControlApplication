e2653f46f61c704f622bfcf7b2882084e4370f60
NikolaiGit <bubiseip@web.de>
2017-12-31 12:28:45 +0100

    added Konstantendeponie.java

---
+++ src/ControlApplication/Konstantendeponie.java
@@ -0,0 +1,6 @@
+package ControlApplication;
+
+public interface Konstantendeponie {
+
+	public final String dest = "C:\\Users\\Nikolai\\Documents\\DHBW\\5.Semester\\Robotik\\Git Repo\\Robots\\bin\\TestRobot1";
+}
